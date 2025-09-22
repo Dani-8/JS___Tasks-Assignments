cf12d73b581acdc3a09fcf66d35d351d751f960f
Dani-8 <daniyalw6190@gmail.com>
2025-09-22 23:32:20 +0500

    minor update

---
+++ assets/ALL Tasks/Ramdan CountDown/js.js
@@ -0,0 +1,13 @@
+function calculateDaysLeft(){
+    let displayDays = document.getElementById("display-days")
+
+    let ramzanStartDate = new Date("Feb 17, 2026")
+    let today = new Date()
+
+    let daysLeft = 
+
+}
+
+
+
+
