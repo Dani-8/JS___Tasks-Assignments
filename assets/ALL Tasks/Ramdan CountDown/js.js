aea6913465ece5ceb1ffaabb6507be603d7506f2
Dani-8 <daniyalw6190@gmail.com>
2025-09-22 23:48:00 +0500

    minor update

--- assets/ALL Tasks/Ramdan CountDown/js.js
+++ assets/ALL Tasks/Ramdan CountDown/js.js
@@ -1,13 +1,25 @@
 function calculateDaysLeft(){
     let displayDays = document.getElementById("display-days")
 
-    let ramzanStartDate = new Date("Feb 17, 2026")
+    let ramzanStartDate = new Date("Feb 17, 2026 00:00:00")
     let today = new Date()
+    
 
-    let daysLeft = 
+    let differenceInTime = ramzanStartDate.getTime() - today.getTime()
+    let differenceInDay = Math.ceil(differenceInTime / (1000 * 3600 * 24))
+    
+
+    if(differenceInDay > 0){
+        displayDays.textContent = differenceInDay
+    }else if(differenceInDay === 0){
+        displayDays.textContent = "Today..."
+    }else{
+        displayDays.textContent = "In progress or passed..."
+    }
+
 
 }
 
 
-
+document.addEventListener("DOMContentLoaded", calculateDaysLeft)
 
