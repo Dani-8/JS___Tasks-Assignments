28527210d51e3c05b39453ca55621ddf90929201
Dani-8 <daniyalw6190@gmail.com>
2025-09-23 23:57:37 +0500

    minor update

--- assets/ALL Tasks/Ramdan CountDown/js.js
+++ assets/ALL Tasks/Ramdan CountDown/js.js
@@ -1,56 +1,56 @@
-function calculateDaysLeft(){
-    let daysLeft = document.getElementById("days-left")
-    let displayDays = document.getElementById("display-days")
-    let note = document.getElementById("note")
-    let toggleButton = document.getElementById("toggle-btn")
+let daysLeft = document.getElementById("days-left")
+let displayDays = document.getElementById("display-days")
+let note = document.getElementById("note")
+let toggleButton = document.getElementById("toggle-btn")
 
-    let detailed = false
+let detailed = false
 
-
+function calculateDaysLeft(){   
     let ramzanStartDate = new Date("Sep 30, 2025 00:00:00")
     let today = new Date()
     
-
     let differenceInTime = ramzanStartDate.getTime() - today.getTime()
     let differenceInDay = Math.ceil(differenceInTime / (1000 * 3600 * 24))
     
+
 
     if(differenceInDay <= 0){
         displayDays.textContent = "Ramadan Mubarak!..."
         daysLeft.classList.add("hidden")
         toggleButton.classList.add("hidden")
         note.classList.add("hidden")
+        return
     }
 
 
-        if(detailed){
-            let years = Math.floor(differenceInDay / 365)
-            let months = Math.floor((differenceInDay % 365) / 30)
-            let days = (differenceInDay % 365) % 30
+    if(detailed){
+        let years = Math.floor(differenceInDay / 365)
+        let months = Math.floor((differenceInDay % 365) / 30)
+        let days = (differenceInDay % 365) % 30
 
-            displayDays.innerHTML = `
-            ${years > 0 ? `<span>${years} <small>Years</small></span>` : ''}
-            ${months > 0 ? `<span>${months} <small>Months</small></span>` : ''}
-            ${days > 0 ? `<span>${days} <small>Days</small></span>` : ''}
-
-            `
-        }else{
-            displayDays.textContent = `${differenceInDay} Days`
-        }
-
-
-    toggleButton.addEventListener("click", function(){
-        detailed = !detailed
-        if(detailed){
-            toggleButton.textContent = 'Show Simple Countdown';
-        }else {
-            toggleButton.textContent = 'Show Detailed Countdown';
-        }
-    })
-
-
+        displayDays.innerHTML = `
+        ${years > 0 ? `<span>${years} <small>Years</small></span>` : ''}
+        ${months > 0 ? `<span>${months} <small>Months</small></span>` : ''}
+        ${days > 0 ? `<span>${days} <small>Days</small></span>` : ''}
+        `
+    }else{
+        displayDays.textContent = `${differenceInDay} Days`
+    }
 }
 
 
-document.addEventListener("DOMContentLoaded", calculateDaysLeft)
+toggleButton.addEventListener("click", function(){
+    detailed = !detailed
+    if(detailed){
+        toggleButton.textContent = 'Show Simple Countdown';
+    }else {
+        toggleButton.textContent = 'Show Detailed Countdown';
+    }
+    calculateDaysLeft()
+})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(calculateDaysLeft, 1000);
+});
 
