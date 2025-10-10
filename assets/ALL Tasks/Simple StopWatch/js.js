72a610b162535536eb8a066cd0c20aeac55f0344
Dani-8 <daniyalw6190@gmail.com>
2025-10-10 23:50:39 +0500

    minor update

--- assets/ALL Tasks/Simple StopWatch/js.js
+++ assets/ALL Tasks/Simple StopWatch/js.js
@@ -2,9 +2,20 @@ let display = document.getElementById("display")
 let startBTN = document.getElementById("start-btn")
 let resetBTN = document.getElementById("reset-btn")
 
+let seconds = 0;
 
 
+function timeFormat(sec){
+    let hrs = String(Math.floor(sec / 3600))
+    let mints = String(Math.floor(sec % 3600))
+    let secs = String(Math.floor(sec % 60))
 
+    return `${hrs}:${mints}:${secs}`
+}
+
+function UpdateDisplay(){
+    display.textContent = timeFormat(seconds)
+}
 
 
 
