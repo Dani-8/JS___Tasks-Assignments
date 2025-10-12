a59724a1fc00cd142237df1975e4975b5b1c4a67
Dani-8 <daniyalw6190@gmail.com>
2025-10-12 16:24:47 +0500

    minor update

--- assets/ALL Tasks/Simple StopWatch/js.js
+++ assets/ALL Tasks/Simple StopWatch/js.js
@@ -3,36 +3,58 @@ let startBTN = document.getElementById("start-btn")
 let resetBTN = document.getElementById("reset-btn")
 
 let seconds = 0;
+let running = false
 
+function timeFormat(ms){
+    let milliSeconds = String(Math.floor((ms % 1000)) / 10).padStart(2, "0")
+    let totalSeconds = String(Math.floor(ms / 1000))
+    let secs = String(Math.floor(totalSeconds % 60)).padStart(2, "0")
+    let mints = String(Math.floor((totalSeconds / 60) % 60)).padStart(2, "0")
+    let hrs = String(Math.floor(totalSeconds / 3600)).padStart(2, "0")
 
-function timeFormat(sec){
-    let hrs = String(Math.floor(sec / 3600)).padStart(2, "0")
-    let mints = String(Math.floor((sec % 3600) / 60)).padStart(2, "0")
-    let secs = String(Math.floor(sec % 60)).padStart(2, "0")
-
-    return `${hrs}:${mints}:${secs}`
+    return `${hrs}:${mints}:${secs}.<span class="milli">${milliSeconds}</span>`;
 }
 
 function UpdateDisplay(){
-    display.textContent = timeFormat(seconds)
+    seconds += 10
+    display.innerHTML = timeFormat(seconds)
 }
 
 
 
-startBTN.addEventListener("click", () => {
-    timer = setInterval(() => {
-        seconds++
-        UpdateDisplay()
-    }, 5);
+// startBTN.addEventListener("click", () => {
+//     timer = setInterval(UpdateDisplay, 10);
 
-})
+// })
 
+function startStopWatch(){
+    if(running){
+        running = false
+        clearInterval(timer)
+        
+        startBTN.textContent = "Resume"
+        startBTN.classList.remove('pause-btn');
+        startBTN.classList.add('start-btn');
+
+    }else{
+        running = true
+
+        timer = setInterval(UpdateDisplay, 10);
+
+        startBTN.textContent = "Pause"
+        startBTN.classList.remove('start-btn');
+        startBTN.classList.add('pause-btn');
+        
+    }
+}
+startBTN.addEventListener("click", startStopWatch)
 
 resetBTN.addEventListener("click", () => {
     clearInterval(timer)
     seconds = 0
     UpdateDisplay()
 })
+
 
 
 
