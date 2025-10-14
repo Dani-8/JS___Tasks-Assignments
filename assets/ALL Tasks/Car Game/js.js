1f0a593ac39d2ae8457ee9fa07be70a5d9d44669
Dani-8 <daniyalw6190@gmail.com>
2025-10-14 23:26:38 +0500

    minor update

--- assets/ALL Tasks/Car Game/js.js
+++ assets/ALL Tasks/Car Game/js.js
@@ -1,14 +1,20 @@
 let car = document.getElementById("car")
 // ------------------------------------------------------------
 
+let CAR_WIDTH = 60
+let CAR_HEIGHT = 100
+let ROAD_WIDTH = 445
+let ROAD_HEIGHT = 700
+
+
 let CAR_STOP_Y = 580
 let CAR_FAST_Y = 150
 let ACCEL_RATE = 3.0
-let STEERING_SPEED = 10
-let CAR_X = 0
+let STEERING_SPEED = 5
+let MAX_CAR_X = ROAD_WIDTH - CAR_WIDTH
 
-let carX
 let carTop = CAR_STOP_Y
+let carX = (ROAD_WIDTH / 2) - (CAR_WIDTH / 2)
 let keys = {}
 // ------------------------------------------------------------
 
@@ -17,6 +23,7 @@ let keys = {}
 
 
 function game(){
+    // TOP & DOWN
     if(keys["ArrowUp"]){
         carTop = Math.max(CAR_FAST_Y, carTop - ACCEL_RATE);
     }
@@ -24,13 +31,13 @@ function game(){
         carTop = Math.min(CAR_STOP_Y, carTop + ACCEL_RATE);
     }
     car.style.top = carTop + 'px';
-
-
-    if(keys["ArrowLeft"]){
+    // ------------------------------------------------------
+    // LEFT & RIGHT
+    if (keys['ArrowLeft']) {
         carX = Math.max(0, carX - STEERING_SPEED);
     }
-    if(keys["ArrowRight"]){
-        carX = Math.min(CAR_X, carX + STEERING_SPEED);
+    if (keys['ArrowRight']) {
+        carX = Math.min(MAX_CAR_X, carX + STEERING_SPEED);
     }
     car.style.left = carX + 'px';
     
