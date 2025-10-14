55b46ae66715b0d71821de8377929742e11260ef
Dani-8 <daniyalw6190@gmail.com>
2025-10-14 22:12:15 +0500

    minor update

--- assets/ALL Tasks/Car Game/js.js
+++ assets/ALL Tasks/Car Game/js.js
@@ -1,12 +1,36 @@
+let car = document.getElementById("car")
 
 
+let keys = {}
 
+// function game(){
+//     if(keys["ArrowUp"]){
+//         console.log("sdfsdf");
+        
+//     }
+    
 
+// }
+// game()
 
+// document.addEventListener("keydown", (e) => {
+//     if(e.key === "ArrowLeft" || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
+//         keys[e.key] = true
+//         e.preventDefault()
+//     }
+// })
+// document.addEventListener("keyup", (e) => {
+//     if(e.key === "ArrowLeft" || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
+//         keys[e.key] = false
+//     }
+// })
 
+function www(e){
+    console.log(e);
+    
+}
 
-
-
+www()
 
 
 
