481b95e11884ea9b2bdc21d09ed932d37d21838d
Dani-8 <daniyalw6190@gmail.com>
2025-10-14 22:28:09 +0500

    minor update

--- assets/ALL Tasks/Car Game/js.js
+++ assets/ALL Tasks/Car Game/js.js
@@ -3,34 +3,32 @@ let car = document.getElementById("car")
 
 let keys = {}
 
-// function game(){
-//     if(keys["ArrowUp"]){
-//         console.log("sdfsdf");
+function game(){
+    if(keys["ArrowUp"]){
         
-//     }
+    }
     
 
-// }
-// game()
-
-// document.addEventListener("keydown", (e) => {
-//     if(e.key === "ArrowLeft" || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
-//         keys[e.key] = true
-//         e.preventDefault()
-//     }
-// })
-// document.addEventListener("keyup", (e) => {
-//     if(e.key === "ArrowLeft" || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
-//         keys[e.key] = false
-//     }
-// })
-
-function www(e){
-    console.log(e);
-    
+}
+// Run the game loop continuously so key state is checked each frame
+function loop(){
+    game()
+    requestAnimationFrame(loop)
 }
 
-www()
+loop()
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft" || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
+        keys[e.key] = true
+        e.preventDefault()
+    }
+})
+document.addEventListener("keyup", (e) => {
+    if(e.key === "ArrowLeft" || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'){
+        keys[e.key] = false
+    }
+})
 
 
 
