c48c979117be77408c4f9b78e8f2566cfb85f16a
Dani-8 <daniyalw6190@gmail.com>
2025-10-14 22:58:41 +0500

    minor update

--- assets/ALL Tasks/Car Game/js.js
+++ assets/ALL Tasks/Car Game/js.js
@@ -1,12 +1,39 @@
 let car = document.getElementById("car")
+// ------------------------------------------------------------
 
+let CAR_STOP_Y = 580
+let CAR_FAST_Y = 150
+let ACCEL_RATE = 3.0
+let STEERING_SPEED = 10
+let CAR_X = 0
 
+let carX
+let carTop = CAR_STOP_Y
 let keys = {}
+// ------------------------------------------------------------
+
+
+
+
 
 function game(){
     if(keys["ArrowUp"]){
-        
+        carTop = Math.max(CAR_FAST_Y, carTop - ACCEL_RATE);
     }
+    if(keys["ArrowDown"]){
+        carTop = Math.min(CAR_STOP_Y, carTop + ACCEL_RATE);
+    }
+    car.style.top = carTop + 'px';
+
+
+    if(keys["ArrowLeft"]){
+        carX = Math.max(0, carX - STEERING_SPEED);
+    }
+    if(keys["ArrowRight"]){
+        carX = Math.min(CAR_X, carX + STEERING_SPEED);
+    }
+    car.style.left = carX + 'px';
+    
     
 
 }
