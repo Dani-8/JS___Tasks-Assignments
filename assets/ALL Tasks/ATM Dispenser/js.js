5278c88d2c92ab073b2b1e2a46c013437b9e35ff
Dani-8 <daniyalw6190@gmail.com>
2025-10-18 23:57:23 +0500

    minor update

--- assets/ALL Tasks/ATM Dispenser/js.js
+++ assets/ALL Tasks/ATM Dispenser/js.js
@@ -1,8 +1,15 @@
+function withdraw(){
+    let amount = parseInt(document.getElementById("amount").value)
+    let result = document.getElementById("result")
+
+    let notes = [5000, 1000, 500, 100, 50, 20, 10]
+    
 
 
+}
 
-
-
+let btn = document.getElementById("btn")
+btn.addEventListener("click", withdraw)
 
 
 
