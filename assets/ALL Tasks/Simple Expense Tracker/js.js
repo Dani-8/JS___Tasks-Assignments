5ee7ca311bc5cd13555c2e60cf7cdccdf63d8528
Dani-8 <daniyalw6190@gmail.com>
2025-11-16 22:30:01 +0500

    Updates

--- assets/ALL Tasks/Simple Expense Tracker/js.js
+++ assets/ALL Tasks/Simple Expense Tracker/js.js
@@ -1,11 +1,14 @@
-    
+let balance = document.getElementById("balance")
+let moneyPlue = document.getElementById("money-plus")
+let moneyMinus = document.getElementById("money-minus")
 
+let titleInput = document.getElementById("title-input")
+let categoryInput = document.getElementById("category-input")
+let dateInput = document.getElementById("date-input")
+let incomeAmountInput = document.getElementById("income-amount-input")
+let expenseAmountInput = document.getElementById("expense-amount-input")
 
-
-
-
-
-
+let listCont = document.getElementById("list-cont")
 
 
 
