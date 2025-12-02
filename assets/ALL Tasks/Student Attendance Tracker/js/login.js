7249dc4affcb45207c4d8141254bc76a9d6a25cd
Dani-8 <daniyalw6190@gmail.com>
2025-12-02 20:27:36 +0500

    minor update

---
+++ assets/ALL Tasks/Student Attendance Tracker/js/login.js
@@ -0,0 +1,14 @@
+export let renderLoginForm = () => {
+    return `
+        
+    `
+
+
+}
+
+
+
+
+
+
+
---
+++ assets/ALL Tasks/Student Attendance Tracker/js/utils.js
@@ -0,0 +1,40 @@
+export let todayDate = new Date().toISOString().split("T")[0]
+// console.log(todayDate)
+// ---------------------------------------------------
+
+export let studentData = []
+export let studentsAttendance = {}
+export let isDataLoaded = false
+export let allDates = []
+// --------------------------------
+
+export let loadCSVData = (csv) => {
+    let lines 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
