55e402b8b8fa5470de7f6cf599c64c79a9714483
Dani-8 <daniyalw6190@gmail.com>
2025-12-29 04:48:47 +0500

    Updates

---
+++ assets/ALL Tasks/Simple Weather App/Weather App Premium/api.js
@@ -0,0 +1,57 @@
+import { apiKey, currentUnit, statusMSG } from "./main.js";
+import { updateUI } from "./main";
+
+
+export async function fetchWeather(city){
+    if (city === ""){
+        alert("Please enter a city name.")
+        return
+    }
+    // -------------------------------------
+
+    lastSearchedCity = city
+    statusMSG.textContent = "SEARCHING...";
+    statusMSG.classList.remove('hidden')
+
+
+    try{
+        let [res, fres] = await Promise.all([
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${currentUnit}&appid=${apiKey}`),
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${currentUnit}&appid=${apiKey}`)
+        ])
+
+        if(!res.ok){
+            throw new Error("City not found")
+        }
+
+        let data = await res.json()
+        let fData = await fres.json()
+        updateUI(data, fData)
+    }catch(err){
+        statusMSG.textContent = err.message.toUpperCase()
+        setTimeout(() => {
+            statusMSG.classList.add('hidden')
+        }, 2500);
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/ALL Tasks/Simple Weather App/Weather App Premium/main.js
+++ assets/ALL Tasks/Simple Weather App/Weather App Premium/main.js
@@ -1,3 +1,29 @@
+import { fetchWeather } from "./api.js"
+// ---------------------------------------------------------
+
+export let apiKey = "345dcc38843fc6a31c10ba8609e3f255"
+export let currentUnit = 'metric';
+export let lastSearchedCity = '';
+export let tChart = null;
+export let rChart = null;
+// ---------------------------------------------------------
+
+export let mainCard = document.getElementById('main-card')
+export let header = document.getElementById('header')
+export let cityInput = document.getElementById('city-input')
+export let searchBTN = document.getElementById('search-btn')
+export let unitToggle = document.getElementById("unit-toggle")
+
+export let statusMSG = document.getElementById("status-msg")
+export let weatherCont = document.getElementById("weather-cont")
+export let placeholder = document.getElementById("placeholder")
+// ---------------------------------------------------------
+
+
+export function updateUI(data, fData){
+    
+
+}
 
 
 
