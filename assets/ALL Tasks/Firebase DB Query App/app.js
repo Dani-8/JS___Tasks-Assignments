e6f46d2421e4a71d49e20caa32bae6b703b8bb78
Dani-8 <daniyalw6190@gmail.com>
2026-01-18 00:57:43 +0500

    update

--- assets/ALL Tasks/Firebase DB Query App/app.js
+++ assets/ALL Tasks/Firebase DB Query App/app.js
@@ -1,14 +1,14 @@
 
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA37Fz9gJ0k9OG9D1VQ82a7mHR6sHBF2sw",
-    authDomain: "shopping-list-a71ae.firebaseapp.com",
-    projectId: "shopping-list-a71ae",
-    storageBucket: "shopping-list-a71ae.firebasestorage.app",
-    messagingSenderId: "31851530734",
-    appId: "1:31851530734:web:6b5a6f5edc3d41bb5fe6ff"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA37Fz9gJ0k9OG9D1VQ82a7mHR6sHBF2sw",
+//     authDomain: "shopping-list-a71ae.firebaseapp.com",
+//     projectId: "shopping-list-a71ae",
+//     storageBucket: "shopping-list-a71ae.firebasestorage.app",
+//     messagingSenderId: "31851530734",
+//     appId: "1:31851530734:web:6b5a6f5edc3d41bb5fe6ff"
+// };
 
 
 
