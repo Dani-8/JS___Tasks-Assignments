b0dfefa4fa8e672264aafd72f5cbc73baa1e2e4e
Dani-8 <daniyalw6190@gmail.com>
2026-01-19 10:39:15 +0500

    update

--- assets/ALL Tasks/Firebase DB Query App/app.js
+++ assets/ALL Tasks/Firebase DB Query App/app.js
@@ -26,25 +26,104 @@ let nameInput = document.getElementById('item-name')
 let catInput = document.getElementById('item-cat')
 let addBtn = document.getElementById('add-btn')
 let listDiv = document.getElementById('list')
+let statusPeek = document.getElementById('status-peek');
 
-let editBtn = document.getElementById('edit-btn')
-let delBtn = document.getElementById('del-btn')
+
+let modal = document.getElementById('edit-modal');
+let modalInput = document.getElementById('modal-input');
+let modalSave = document.getElementById('modal-save');
 
 let currentEditId = null
 // ---------------------------------------------------------
 
+function showPeek(msg, type) {
+    statusPeek.textContent = msg
+    statusPeek.className = `status-peek active ${type}`
+
+    setTimeout(() => {
+        statusPeek.classList.remove('active')
+    }, 1000);
+}
+// ---------------------------------
+
+
 function addToList() {
     if (!nameInput.value.trim()) return;
+
 
     addDoc(listRef, {
         name: nameInput.value.trim(),
         category: catInput.value,
         createdAt: serverTimestamp(),
         bought: false
-    }).then(() => { 
+    }).then(() => {
+        showPeek('Item Added!', 'add')
         nameInput.value = ''
     })
+}
+addBtn.addEventListener('click', addToList)
+// -----------------------------------
 
+
+function updateTheList() {
+    let q = query(listRef, orderBy('createdAt', 'desc'))
+
+
+    onSnapshot(q, function (snapshot) {
+        listDiv.innerHTML = ''
+        snapshot.forEach((docSnap) => {
+            let data = docSnap.data()
+            let id = docSnap.id
+
+            let itemDiv = document.createElement('div')
+            itemDiv.className = `item-row ${data.bought ? 'bought' : ''}`
+            // itemDiv.addEventListener('dblclick', () => {
+            //     window.toggleItem(docSnap.id, data.bought)
+            // })
+
+            itemDiv.innerHTML = `
+                <div class="item-info">
+                    <span class="item-name">${data.name}</span>
+                    <span class="item-meta">${data.category}</span>
+                </div>
+                <div class="actions">
+                    <button class="action-btn btn-edit">
+                        <i data-lucide="edit-3"></i>
+                    </button>
+                    <button class="action-btn btn-del">
+                        <i data-lucide="x"></i>
+                    </button>
+                </div>
+            `
+            listDiv.appendChild(itemDiv)
+
+
+            // EDIT ITEM
+            let editBtn = itemDiv.querySelector('.btn-edit')
+            editBtn.addEventListener('click', () => itemEdited(id))
+
+            // DELETE ITEM
+            let delBtn = itemDiv.querySelector('.btn-del')
+            delBtn.addEventListener('click', () => itemDeleted(id))
+        })
+        lucide.createIcons()
+
+
+    })
+
+
+}
+updateTheList()
+
+
+
+
+function itemEdited(id) {
+    console.log('EDIT ITEM ID:', id);
+}
+
+function itemDeleted(id) {
+    console.log('DELETE ITEM ID:', id);
 }
 
 
@@ -56,15 +135,6 @@ function addToList() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 lucide.createIcons()
+
+
